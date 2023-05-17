ab672c577bdfa322fd359bf4e20b47081b411e3b
lucny <lucny@sspu-opava.cz>
2023-05-17 22:56:20 +0200

    model Film - validace

--- movies/models.py
+++ movies/models.py
@@ -1,8 +1,13 @@
 # Import knihovny models (součást balíčku django.db), která obsahuje programové prostředky
 # pro vytváření modelů
+import datetime
+
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, RegexValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 # Import vestavěného modulu os, který obsahuje různé utility pro práci s daným OS
 import os
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Modely-třídy, které tvoří datovou strukturu aplikace
@@ -24,15 +29,17 @@ class Genre(models.Model):
     ve formuláři se bude zobrazovat pod označením "Genre name" (parametr verbose_name),
     a uživateli se jako nápověda nabídne text uvedený v parametru help_text
     '''
-    name = models.CharField(max_length=50,
-                            unique=True,
-                            verbose_name='Název žánru',
-                            help_text='Zadejte označení filmového žánru (např. sci-fi, komedie)')
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name='Název žánru',
+        help_text='Zadejte označení filmového žánru (např. sci-fi, komedie)')
 
     '''
     Metadata - slouží ke specifikaci některých dalších vlastností modelu, 
     jež ale už mohou být v řadě případů závislé i na konkrétním typu použité databáze 
     '''
+
     class Meta:
         # Popisek používaný při pojmenování jednoho záznamu
         verbose_name = 'Žánr'
@@ -45,6 +52,7 @@ class Genre(models.Model):
     '''
     Methods (metody) určují chování objektu v určité situaci  
     '''
+
     def __str__(self):
         '''
         Magická metoda __str__() vrací řetězec, který se používá jako textová reprezentace objektu
@@ -61,42 +69,66 @@ class Film(models.Model):
     '''
     # Fields
     # Znakové pole o maximální délce 200 znaků pro vložení názvu filmu
-    title = models.CharField(max_length=200,
-                             verbose_name='Název filmu',
-                             help_text='Zadejte název filmu')
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Název filmu',
+        help_text='Zadejte název filmu',
+        # Nastavení dvou validačních podmínek - minimální délka řetězce a kontrola podle regex
+        validators=[
+            MinLengthValidator(2, 'Název nesmí být kratší než 2 znaky!'),
+            RegexValidator(r'^[^+\-*/%&;:.,]', 'Název nesmí začínat speciálním znakem!')
+        ]
+    )
     # Textové pole pro vložení delšího textu popisujícího děj filmu
     # Formulářový prvek nemusí je nepovinný - atribut null=True
     # a textové pole nemusí být vyplněno - atribut blank=True
-    plot = models.TextField(null=True,
-                            blank=True,
-                            verbose_name='Děj')
+    plot = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Děj')
     # Pole obsahuje datum uvedení filmu, které musí být zadáno v náležitém tvaru (YYYY-MM-DD); nepovinný údaj
-    release_date = models.DateField(null=True,
-                                    blank=True,
-                                    verbose_name='Datum uvedení',
-                                    help_text='Zadejte, prosím, datum ve formátu: <em>den.měsíc.rok</em>')
+    release_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Datum uvedení',
+        help_text='Zadejte, prosím, datum ve formátu: <em>den.měsíc.rok</em>')
     # Celočíselné pole pro nepovinné zadání stopáže (délky) filmu v minutách
-    runtime = models.PositiveSmallIntegerField(null=True,
-                                  blank=True,
-                                  verbose_name='Délka filmu',
-                                  help_text='Prosím, zadejte celočíselný údaj vyjadřující délku filmu v minutách')
+    runtime = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True,
+        verbose_name='Délka filmu',
+        help_text='Prosím, zadejte celočíselný údaj vyjadřující délku filmu v minutách',
+        # Nastavení číselného validátoru
+        validators=[
+            MaxValueValidator(1000, 'Hodnota nesmí být větší než 1000!'),
+        ]
+    )
     # Pole pro zadání desetinného čísla pro hodnocení filmu v rozsahu 0.0 až 10.0
     # Výchozí hodnota je nastavena na 5.0
     # K validaci hodnot jsou použity metody z balíku/knihovny django.core.validators
-    rating = models.FloatField(default=5.0,
-                             null=True,
-                             verbose_name='Hodnocení',
-                             help_text='Zadejte desetinné číslo v rozsahu 0.0 až 10.0')
+    rating = models.FloatField(
+        default=5.0,
+        null=True,
+        verbose_name='Hodnocení',
+        help_text='Zadejte desetinné číslo v rozsahu 0.0 až 10.0',
+        # Nastavení číselných validátorů
+        validators=[
+            MinValueValidator(0, 'Hodnota nesmí být menší než 0!'),
+            MaxValueValidator(10, 'Hodnota nesmí být větší než 10!'),
+        ]
+    )
     # Pole typu image, které umožňuje upload obrázku s plakátem filmu
-    poster = models.ImageField(upload_to='posters',
-                               blank=True,
-                               null=True,
-                               verbose_name='Plakát k filmu',
-                               help_text='Zde můžete přiložit grafický soubor s filmovým plakátem')
+    poster = models.ImageField(
+        upload_to='posters',
+        blank=True,
+        null=True,
+        verbose_name='Plakát k filmu',
+        help_text='Zde můžete přiložit grafický soubor s filmovým plakátem')
     # Vytvoří vztah mezi modely Film a Genre typu M:N
-    genres = models.ManyToManyField(Genre,
-                                    verbose_name='Žánry',
-                                    help_text='K označení většího počtu žánrů můžete stisknout klávesu CTRL')
+    genres = models.ManyToManyField(
+        Genre,
+        verbose_name='Žánry',
+        help_text='K označení většího počtu žánrů můžete stisknout klávesu CTRL')
 
     # Metadata
     class Meta:
@@ -113,6 +145,22 @@ class Film(models.Model):
         '''
         return f'{self.title} ({str(self.release_date.year)}), hodnocení: {str(self.rating)}'
 
+    def clean(self):
+        '''
+        Metoda clean() v modelu Django slouží k validaci a upravě dat před uložením do databáze.
+        Je volána automaticky při volání metody save() na instanci modelu.
+        '''
+        # První validační podmínka kontroluje, zda hodnota year v release_date je menší než 1895.
+        # Pokud je tato podmínka splněna, vyvolá se výjimka ValidationError s příslušnou chybovou zprávou.
+        # Tím se zamezí uložení filmu s neplatným datem uvedení.
+        if (self.release_date.year < 1895):
+            raise ValidationError({'release_date': 'Datum uvedení filmu nemůže být před rokem 1895!'})
+        # Druhá podmínka kontroluje, zda hodnota release_date je větší než aktuální datum (datetime.date.today()).
+        # Pokud je tato podmínka splněna, opět se vyvolá výjimka ValidationError s příslušnou chybovou zprávou,
+        # aby se zabránilo uložení filmu s datem uvedení v budoucnosti.
+        if (self.release_date > datetime.date.today()):
+            raise ValidationError({'release_date': 'Datum uvedení filmu nemůže být v budoucnosti!'})
+
 
 class Attachment(models.Model):
     '''
@@ -120,21 +168,24 @@ class Attachment(models.Model):
     '''
     # Fields
     # Povinný titulek přílohy - text do délky 200 znaků
-    title = models.CharField(max_length=200,
-                             verbose_name='Titulek přílohy',
-                             help_text='Doplňte stručný popis přílohy (do 200 znaků)')
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Titulek přílohy',
+        help_text='Doplňte stručný popis přílohy (do 200 znaků)')
     # Časový údaj o poslední aktualizaci přílohy - automaticky se ukládá aktuální čas
     last_update = models.DateTimeField(auto_now=True)
+
     # Pole pro upload souboru
     def attachment_path(self, filename):
         ''' Pomocná metoda, která zajistí uložení přílohy do složky attachments a podsložky označené podle id filmu '''
         return os.path.join('attachments', str(self.film.id), filename)
 
     # Parametr upload_to zajistí uložení souboru do složky specifikované v návratové hodnotě metody attachment_path
-    file = models.FileField(upload_to=attachment_path,
-                            null=True,
-                            verbose_name='Příloha',
-                            help_text='Vložte soubor s požadovanou přílohou')
+    file = models.FileField(
+        upload_to=attachment_path,
+        null=True,
+        verbose_name='Příloha',
+        help_text='Vložte soubor s požadovanou přílohou')
     # Konstanta, v níž jsou ve formě n-tic (tuples) předdefinovány různé typy příloh
     TYPE_OF_ATTACHMENT = (
         ('audio', 'audio'),
@@ -144,19 +195,22 @@ class Attachment(models.Model):
         ('jiná', 'jiná'),
     )
     # Pole s definovanými předvolbami pro uložení typu přílohy
-    type = models.CharField(max_length=10,
-                            choices=TYPE_OF_ATTACHMENT,
-                            blank=True,
-                            default='obrázek',
-                            verbose_name='Typ přílohy',
-                            help_text='Vyberte některý z uvedených typů příloh')
+    type = models.CharField(
+        max_length=10,
+        choices=TYPE_OF_ATTACHMENT,
+        blank=True,
+        default='obrázek',
+        verbose_name='Typ přílohy',
+        help_text='Vyberte některý z uvedených typů příloh')
     # Cizí klíč, který zajišťuje propojení přílohy s daným filmem (vztah N:1)
     # Parametr on_delete slouží k zajištění tzv. referenční integrity - v případě odstranění filmu
     # budou odstraněny i všechny jeho přílohy (models.CASCADE)
-    film = models.ForeignKey(Film,
-                             on_delete=models.CASCADE,
-                             verbose_name='Film',
-                             help_text='Vyberte film, s nímž má být příloha spojena')
+    film = models.ForeignKey(
+        Film,
+        on_delete=models.CASCADE,
+        verbose_name='Film',
+        help_text='Vyberte film, s nímž má být příloha spojena')
+
     # Metadata
     class Meta:
         verbose_name = 'Příloha'
